c5948605389ebe8dc940eeeb25cb3ffa46cdb418
brinkqiang <brink.qiang@gmail.com>
2018-08-02 13:30:19 +0800

    add

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,38 @@
 
-#include "dmos.h"
-#include "dmutil.h"
-#include "dmtypes.h"
+#include <stdio.h>
+#include <malloc.h>
 
-int main(int argc, char* argv[]) {
+struct elem
+{
+    int a;
+    double b;
+    char* c;
+};
 
-    std::cout << "hello world!" << std::endl;
+int main()
+{
+    char* p = (char*)malloc(sizeof(char) * 10);
+    if (p == NULL) {
+        return 0;
+    }
+
+    struct elem* e = (struct elem*)malloc(sizeof(struct elem));
+    if (e == NULL) {
+        free(p);
+        return 0;
+    }
+
+    e->a = 10;
+    e->b = 10.10;
+    e->c = p;
+
+    double *xx = &e->b;
+
+    printf("%f\n", *xx);
+
+    free(e);
+
+    printf("%f\n", *xx);
+
     return 0;
 }
