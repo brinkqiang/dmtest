c1e4a0b6c12a21316f22a31547fe0e4e789db2ce
brinkqiang <brink.qiang@gmail.com>
2018-09-12 09:10:36 +0800

    add

---
+++ test/dmtesttest.cpp
@@ -0,0 +1,22 @@
+
+#include "dmos.h"
+#include "dmutil.h"
+#include "dmtypes.h"
+#include "dmformat.h"
+#include <gtest/gtest.h>
+
+TEST(dmformat, dmformat) {
+    std::cout << fmt::format("{0},{1},{2}", "a", "b", "c") << std::endl;
+    std::cout << fmt::format("{2},{1},{0}", "a", "b", "c") << std::endl;
+    std::cout << fmt::format("{:%Y-%m-%d %H:%M:%S}", fmt::localtime(time(NULL))) << std::endl;
+
+    std::cout << fmt::format("{}{}{}{}{}{}{}{}{}{}{}{}",
+        1, 2, 3, 4, 5, 6, 'a', 'b', 'c', 'd', 'e', 'f') << std::endl;
+
+    std::cout << fmt::format("{_1}/{a_}/{A_}", fmt::arg("a_", 'a'),
+        fmt::arg("A_", "A"), fmt::arg("_1", 1)) << std::endl;
+
+    char name[] = "fmt";
+    std::string strName = "{}";
+    strName = fmt::format(strName, name);
+}
