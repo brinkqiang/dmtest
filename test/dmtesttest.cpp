f025047170cf510ff2c648ab9099bac89fae49d3
brinkqiang <brink.qiang@gmail.com>
2018-12-02 00:08:34 +0800

    add

--- test/dmtesttest.cpp
+++ test/dmtesttest.cpp
@@ -6,18 +6,36 @@
 #include "dmformat.h"
 #include "dmtime.h"
 
+template<typename T>
+struct LuaRead
+{
+    static T Read()
+    {
+        return T();
+    }
+};
+
+template<>
+struct LuaRead<uint64_t>
+{
+    static uint64_t Read()
+    {
+        return 1;
+    }
+};
+
+template<>
+struct LuaRead<int64_t>
+{
+    static int64_t Read()
+    {
+        return 2;
+    }
+};
+
 TEST(dmformat, dmformat) {
-    std::cout << fmt::format("{0},{1},{2}", "a", "b", "c") << std::endl;
-    std::cout << fmt::format("{2},{1},{0}", "a", "b", "c") << std::endl;
-    std::cout << fmt::format("{:%Y-%m-%d %H:%M:%S}", fmt::localtime(time(NULL))) << std::endl;
 
-    std::cout << fmt::format("{}{}{}{}{}{}{}{}{}{}{}{}",
-        1, 2, 3, 4, 5, 6, 'a', 'b', 'c', 'd', 'e', 'f') << std::endl;
+    int i = LuaRead<uint64_t>::Read();
+    int j = LuaRead<uint64_t>::Read();
 
-    std::cout << fmt::format("{_1}/{a_}/{A_}", fmt::arg("a_", 'a'),
-        fmt::arg("A_", "A"), fmt::arg("_1", 1)) << std::endl;
-
-    char name[] = "fmt";
-    std::string strName = "{}";
-    strName = fmt::format(strName, name);
 }
